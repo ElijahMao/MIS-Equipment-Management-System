886d15fb1471cbf489788514919cddf5b9c00020
ElijahMao <40432292+ElijahMao@users.noreply.github.com>
2018-07-31 13:15:26 +0800

    20170731
    完成更新歸還設備的頁面。當選擇不同設備編號，員工編號和員工姓名會自動帶出。

--- return.php
+++ return.php
@@ -55,7 +55,7 @@
                             <div class="form-group">
                                 <div class="form-input">
 									<select class="form-control" style="font-size:36px; height:70px" name="assets_no" id="assets_no" onchange="myFunction()">
-										<option value="no"></option>
+										<option value="empty"></option>
 <?php
 		$his_sql=mysql_query("SELECT ASS_NO FROM history WHERE Return_Date is null ORDER BY ASS_NO");
 		for($i=1;$i<=mysql_num_rows($his_sql);$i++){
@@ -64,29 +64,120 @@
 										<option value="<?php echo $his_data[0] ?>"><?php echo $his_data[0] ?></option>
 <?php
 		}
-?>					
-
-									</select>
-                                </div>
-							</div>
-<?php
-	}
-?>									
-<p id="demo"></p>
+?>										
 <script>
 function myFunction() {
     var x = document.getElementById("assets_no").value;
-    document.getElementById("demo").innerHTML = "You selected: " + x;
+	if(x=="NB01"){
+		<?php
+			$x_sql=mysql_query("SELECT history.EMP_NO,employee.NAME FROM history,employee WHERE history.ASS_NO='NB01' AND history.Return_Date is NULL AND history.EMP_NO=employee.EMP_NO");
+			$x_data=mysql_fetch_row($x_sql);
+		?>
+		document.getElementById("emp_no").value = "<?php echo $x_data[0] ?>";
+		document.getElementById("emp_name").value = "<?php echo $x_data[1] ?>";
+	}else if(x=="NB02"){
+		<?php
+			$x_sql=mysql_query("SELECT history.EMP_NO,employee.NAME FROM history,employee WHERE history.ASS_NO='NB02' AND history.Return_Date is NULL AND history.EMP_NO=employee.EMP_NO");
+			$x_data=mysql_fetch_row($x_sql);
+		?>
+		document.getElementById("emp_no").value = "<?php echo $x_data[0] ?>";
+		document.getElementById("emp_name").value = "<?php echo $x_data[1] ?>";
+	}else if(x=="NB03"){
+		<?php
+			$x_sql=mysql_query("SELECT history.EMP_NO,employee.NAME FROM history,employee WHERE history.ASS_NO='NB03' AND history.Return_Date is NULL AND history.EMP_NO=employee.EMP_NO");
+			$x_data=mysql_fetch_row($x_sql);
+		?>
+		document.getElementById("emp_no").value = "<?php echo $x_data[0] ?>";
+		document.getElementById("emp_name").value = "<?php echo $x_data[1] ?>";
+	}else if(x=="NB04"){
+		<?php
+			$x_sql=mysql_query("SELECT history.EMP_NO,employee.NAME FROM history,employee WHERE history.ASS_NO='NB04' AND history.Return_Date is NULL AND history.EMP_NO=employee.EMP_NO");
+			$x_data=mysql_fetch_row($x_sql);
+		?>
+		document.getElementById("emp_no").value = "<?php echo $x_data[0] ?>";
+		document.getElementById("emp_name").value = "<?php echo $x_data[1] ?>";
+	}else if(x=="SONY01"){
+		<?php
+			$x_sql=mysql_query("SELECT history.EMP_NO,employee.NAME FROM history,employee WHERE history.ASS_NO='SONY01' AND history.Return_Date is NULL AND history.EMP_NO=employee.EMP_NO");
+			$x_data=mysql_fetch_row($x_sql);
+		?>
+		document.getElementById("emp_no").value = "<?php echo $x_data[0] ?>";
+		document.getElementById("emp_name").value = "<?php echo $x_data[1] ?>";
+	}else if(x=="SONY02"){
+		<?php
+			$x_sql=mysql_query("SELECT history.EMP_NO,employee.NAME FROM history,employee WHERE history.ASS_NO='SONY02' AND history.Return_Date is NULL AND history.EMP_NO=employee.EMP_NO");
+			$x_data=mysql_fetch_row($x_sql);
+		?>
+		document.getElementById("emp_no").value = "<?php echo $x_data[0] ?>";
+		document.getElementById("emp_name").value = "<?php echo $x_data[1] ?>";
+	}else if(x=="SONY03"){
+		<?php
+			$x_sql=mysql_query("SELECT history.EMP_NO,employee.NAME FROM history,employee WHERE history.ASS_NO='SONY03' AND history.Return_Date is NULL AND history.EMP_NO=employee.EMP_NO");
+			$x_data=mysql_fetch_row($x_sql);
+		?>
+		document.getElementById("emp_no").value = "<?php echo $x_data[0] ?>";
+		document.getElementById("emp_name").value = "<?php echo $x_data[1] ?>";
+	}else if(x=="SONY04"){
+		<?php
+			$x_sql=mysql_query("SELECT history.EMP_NO,employee.NAME FROM history,employee WHERE history.ASS_NO='SONY04' AND history.Return_Date is NULL AND history.EMP_NO=employee.EMP_NO");
+			$x_data=mysql_fetch_row($x_sql);
+		?>
+		document.getElementById("emp_no").value = "<?php echo $x_data[0] ?>";
+		document.getElementById("emp_name").value = "<?php echo $x_data[1] ?>";
+	}else if(x=="USB01"){
+		<?php
+			$x_sql=mysql_query("SELECT history.EMP_NO,employee.NAME FROM history,employee WHERE history.ASS_NO='USB01' AND history.Return_Date is NULL AND history.EMP_NO=employee.EMP_NO");
+			$x_data=mysql_fetch_row($x_sql);
+		?>
+		document.getElementById("emp_no").value = "<?php echo $x_data[0] ?>";
+		document.getElementById("emp_name").value = "<?php echo $x_data[1] ?>";
+	}else if(x=="USB02"){
+		<?php
+			$x_sql=mysql_query("SELECT history.EMP_NO,employee.NAME FROM history,employee WHERE history.ASS_NO='USB02' AND history.Return_Date is NULL AND history.EMP_NO=employee.EMP_NO");
+			$x_data=mysql_fetch_row($x_sql);
+		?>
+		document.getElementById("emp_no").value = "<?php echo $x_data[0] ?>";
+		document.getElementById("emp_name").value = "<?php echo $x_data[1] ?>";
+	}else if(x=="USB04"){
+		<?php
+			$x_sql=mysql_query("SELECT history.EMP_NO,employee.NAME FROM history,employee WHERE history.ASS_NO='USB04' AND history.Return_Date is NULL AND history.EMP_NO=employee.EMP_NO");
+			$x_data=mysql_fetch_row($x_sql);
+		?>
+		document.getElementById("emp_no").value = "<?php echo $x_data[0] ?>";
+		document.getElementById("emp_name").value = "<?php echo $x_data[1] ?>";
+	}
+	/*
+	//如有新增設備編號，請複製以下程式碼並貼於上方。
+	else if(x=="新增設備編號"){
+		<?php
+			$x_sql=mysql_query("SELECT  history.EMP_NO,employee.NAME FROM history,employee WHERE history.ASS_NO='新增設備編號' AND history.EMP_NO=employee.EMP_NO");
+			$x_data=mysql_fetch_row($x_sql);
+		?>
+		document.getElementById("emp_no").value = "<?php echo $x_data[0] ?>";
+		document.getElementById("emp_name").value = "<?php echo $x_data[1] ?>";
+	}*/
+	else{
+		document.getElementById("emp_no").value = x;
+		document.getElementById("emp_name").value = x;
+	}	
 }
-</script>                                
+</script>							
+									</select>
+                                </div>
+							</div>	
+<?php
+	}
+?>							
+                                
+			
 							<div class="form-group">
                                 <div class="form-input">
-                                    <input type="text" class="form-control" style="font-size:36px; height:70px" name="emp_no" id="emp_no" placeholder="員工編號.." readonly>
+                                    <input type="text" class="form-control" style="font-size:36px; height:70px" name="emp_no" id="emp_no" placeholder="員工編號.." onchange="myFunction()" readonly>
                                 </div>
                             </div>
 							<div class="form-group">
                                 <div class="form-input">
-                                    <input type="text" class="form-control" style="font-size:36px; height:70px" name="emp_name" id="emp_name" placeholder="員工姓名.." readonly>
+                                    <input type="text" class="form-control" style="font-size:36px; height:70px" name="emp_name" id="emp_name" placeholder="員工姓名.." onchange="myFunction()" readonly>
                                 </div>
                             </div>
 							<div class="form-group mb0">
